8f6476a0f1c5a997f00fadbf41eabdf0bcbe9178
avidliznr <37923934+avidliznr@users.noreply.github.com>
2019-06-11 13:30:49 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n2,n1;
+    scanf("%d %d",&n1,&n2);
+    n1++;
+    while(n1<n2){
+        if(n1%2==0){
+            printf("%d ",n1);
+        }
+        n1++;
+    }
+    
+}
