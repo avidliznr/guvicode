2b31312a3290be816fd2aaa4e4a39b8ef7d13947
avidliznr <37923934+avidliznr@users.noreply.github.com>
2019-06-11 13:28:20 +0530

    Update main (4).c

--- main (4).c
+++ main (4).c
@@ -3,9 +3,10 @@ int main()
 {
     int n2,n1;
     scanf("%d %d",&n1,&n2);
-    while(n1!=n2){
+    n1++;
+    while(n1<n2){
         if(n1%2!=0){
-            printf("%d",n1);
+            printf("%d ",n1);
         }
         n1++;
     }
