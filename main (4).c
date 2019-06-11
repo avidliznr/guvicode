692fca2498a3b7c93155d62d1b12fd032c8eb668
avidliznr <37923934+avidliznr@users.noreply.github.com>
2019-06-11 13:26:35 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n2,n1;
+    scanf("%d %d",&n1,&n2);
+    while(n1!=n2){
+        if(n1%2!=0){
+            printf("%d",n1);
+        }
+        n1++;
+    }
+    
+}
