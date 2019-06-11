b7e2b5f9a34bcd7165623d45f9cff19ebe822f8d
avidliznr <37923934+avidliznr@users.noreply.github.com>
2019-06-11 10:01:15 +0530

    Add files via upload

---
+++ guvi3.py
@@ -0,0 +1,10 @@
+char = input()
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
+vov = 'aeiou'
+if char in alphabets:
+	if char in vov:
+		print('Vowel')
+	else:
+		print('Alphabet')
+else:
+	print('invalid')
